5c25539b4a8dfd139e4cf7eed11c84d129b7e297
EnriqueARguez <76457845+EnriqueARguez@users.noreply.github.com>
2023-11-23 07:55:30 +0000

    .

--- Hello.py
+++ Hello.py
@@ -19,6 +19,9 @@ LOGGER = get_logger(__name__)
 
 
 def run():
+    st.write(st.secrets["HOST"])
+    st.write(st.secrets["USER"])
+    st.write(st.secrets["DATABASE"])
     st.set_page_config(
         page_title="Hello",
         page_icon="👋",
